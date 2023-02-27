5b4e982fbe43e193ee670c53c992299ac67d462f
moeinnoghani <mo.noghani@greenweb.ir>
2023-02-27 13:08:11 +0330

    add plugin codes

--- wp-post_footer_text_meta.php
+++ wp-post_footer_text_meta.php
@@ -1,1 +1,21 @@
 <?php
+
+/**
+ * Plugin Name: Post Footer Text Using Meta Field
+ * Plugin URI: https://github.com/moeinnoghani/wordpress-simple-footer-generator
+ * Description: Sends an email notification when an admin user logs in.
+ * Version: 1.0
+ * Author: Moein Noghani
+ * Author URI: https://github.com/moeinnoghani
+ */
+
+function post_footer_text_meta( $content ) {
+    if ( is_singular( 'post' ) ) {
+        $text = get_post_meta( get_the_ID(), 'footer_text', true );
+        if ( $text ) {
+            $content .= '<p>' . $text . '</p>';
+        }
+    }
+    return $content;
+}
+add_filter( 'the_content', 'post_footer_text_meta' );
